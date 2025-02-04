9d3ddad32bda51c904220d0b0a338e32a02ec185
Tanjir alom <normaluser2224@gmail.com>
2025-02-04 22:49:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
     localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
 }
-document.addEventListener("mousemove", function(event) {
-    let shark = document.querySelector(".shark");
-    let x = event.clientX / window.innerWidth * 100;
-    let y = event.clientY / window.innerHeight * 100;
-    shark.style.transform = `translate(${x}vw, ${y}vh)`;
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("darkMode") === "true") {
         document.body.classList.add("dark-mode");
