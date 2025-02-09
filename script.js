5589679f9e97f2ccb72f58016f7fe6da2432ebd3
Tanjir alom <normaluser2224@gmail.com>
2025-02-09 21:34:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,10 @@ button.addEventListener('click', () => {
     isOn = !isOn;
     bulb.style.fill = isOn ? 'rgba(255, 255, 0, 0.9)' : 'rgba(255, 255, 0, 0.2)';
 });
+document.addEventListener("mousemove", function (event) {
+    let bulb = document.getElementById("lightbulb");
+    let x = event.clientX / window.innerWidth - 0.5;
+    let y = event.clientY / window.innerHeight - 0.5;
+    bulb.style.transform = `translateX(-50%) rotate(${x * 20}deg) translateY(${y * 20}px)`;
+});
 
