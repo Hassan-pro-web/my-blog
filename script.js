59d446a2755303dce5c9b7ddd1480442f716194b
Tanjir alom <normaluser2224@gmail.com>
2025-02-09 21:25:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
     localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
 }
+const button = document.getElementById('toggleButton');
+const bulb = document.querySelector('.bulb-glow');
+
+let isOn = false;
+
+button.addEventListener('click', () => {
+    isOn = !isOn;
+    bulb.style.fill = isOn ? 'rgba(255, 255, 0, 0.9)' : 'rgba(255, 255, 0, 0.2)';
+});
 
